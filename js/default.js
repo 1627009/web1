96c097244847ce18df09f1bf121c74035028ee44
1627009 <g1627009@st.t-kougei.ac.jp>
2017-07-10 18:20:00 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 var input = 12345;
 
 var change = function(input){
-  var ten_thousand  = Math.floor(input / 10000);
+/*var ten_thousand  = Math.floor(input / 10000);
   var five_thousand = Math.floor((input / 10000) % 5000);
   var thousand      = Math.floor((input / 10000 / 5000) % 1000);
   var five_hundred  = Math.floor((input / 10000 / 5000 / 1000) % 500);
@@ -9,8 +9,16 @@ var change = function(input){
   var fifty         = Math.floor((input / 10000 / 5000 / 1000 / 500 / 100) % 50);
   var ten           = Math.floor((input / 10000 / 5000 / 1000 / 500 / 100 / 50) % 10);
   var five          = Math.floor((input / 10000 / 5000 / 1000 / 500 / 100 / 50 / 10) % 5);
-  var one           = Math.floor(input / 10000 / 5000 / 1000 / 500 / 100 / 50 / 10 / 5);
-  return [ten_thousand, five_thousand, thousand, five_hundred, hundred, fifty, ten, five, one];
+  var one           = Math.floor(input / 10000 / 5000 / 1000 / 500 / 100 / 50 / 10 / 5);*/
+  var change_number[9];
+  var div_number[9] = {10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
+  var input_rem = input;
+  for(var i = 0;i = 8;i++)
+  {
+    change_number[i] = input_rem / div_number[i];
+    input_rem = div_number[i] * change_number[i];
+  }
+  return [change_number[0], change_number[1], change_number[2], change_number[3], change_number[4], change_number[5], change_number[6], change_number[7], change_number[8]];
 }
 
 var change_anser = change(input);
