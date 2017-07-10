b4b3ccc5184129011e394d9b63af530984e46a1d
1627009 <g1627009@st.t-kougei.ac.jp>
2017-07-10 18:46:00 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,13 +1,15 @@
 var input = 12345;
 
-var change = function(input){
+var change = function(input)
+{
   var change_number[9];
   var div_number[9] = {10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
   var input_rem = input;
-  for(var i = 0; i < 9; i++){
+ /* for(var i = 0; i < 9; i++)
+  {
     change_number[i] = input_rem / div_number[i];
     input_rem -= div_number[i] * change_number[i];
-  }
+  }*/
   return change_number[0];
 }
 
